5495e58e1804b36696fd4d00886a52aa5573fb2b
chris bolivar <christopherbolivar@hotmail.com>
2019-09-26 10:31:38 -0400

    deploying

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const session    = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/vocabapp', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
